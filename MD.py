ccc22c98b2d480b7c484eced3b21133308f51b23
Tomper-dev <65580552+Tomper-dev@users.noreply.github.com>
2020-05-28 21:45:28 -0300

    MagicDungeons ALPHA v1.1.0
    Fixed some errors; last post I added the wrong archive lmao
    
    *ANY BUGS PLEASE LET ME KNOW*

---
+++ MD.py
@@ -0,0 +1,150 @@
+# import section
+from random import choice as ran
+from time import sleep as sle
+
+# variable section
+weapons = [
+    ['Hand', int(5), 0],
+    ['Wooden Sword', 7, 700]
+]
+
+# Monster prefixes: [0]: Name; [1]: Dmg; [2]: Life; [3]: Starter life
+
+Blerg = ['Blerg', 7, 20.0, 20.0]
+
+Orc = ['Orc', 7, 40.0, 40.0]
+
+monster = [
+    Orc,
+    Blerg,
+]
+
+print(len(Orc))
+print(len(Blerg))
+
+pickaxes = [
+    ['Wooden Pickaxe', 0, 600],
+]
+
+axes = [
+    ['Wooden Axe', 0, 500],
+]
+
+inv = []
+
+helmets = [
+    ['Cooper Helmet', 20, 300],
+]
+
+chestplates = [
+    ['Cooper Chestplates', 30, 400],
+]
+
+leggings = [
+    ['Cooper Leggings', 30, 200]
+]
+
+boots = [
+    ['Cooper Boots', 20, 200]
+]
+
+items = [
+    weapons,
+    pickaxes,
+    axes,
+    helmets,
+    chestplates,
+    leggings,
+    boots
+]
+
+stats = {
+    'Name': str(input('Name:! ')),
+    'Level': 1,
+
+    'xp': 0,
+    'MaxXp': 50,
+
+    'Life': 50,
+    'MaxLife': 50,
+
+    'Helmet': None,
+    'Chestplate': None,
+    'Leggings': None,
+    'Boots': None,
+
+    'Axe': None,
+    'Pickaxe': None,
+    'Weapon': weapons[0][0],
+    'Weapon Damage': weapons[0][1],
+    'Active Equipment': None,
+
+    'Money': 100.00,
+    'Defeated': 0
+}
+
+life = stats['Life']
+
+
+# function section
+
+def levelup(levelvar, life, money, maxxpvar, xpvar):
+    levelvar += 1
+    life += 50
+    money += 500
+    maxxpvar += 50
+    xpvar -= stats['MaxXp']
+
+
+# Principal game section
+
+print(f'DESTINY'.center(30, '='))
+sle(1)
+while True:
+    option = str(input('Command ("Wiki" for available commands): ').upper())
+    if option == 'WIKI':
+        print("""
+            Hunt - Search and battle a monster!
+            Mine - Dig to find ores, money and XP! (Pickaxe required) DEVELOPING
+            Chop - Cut wood to get wood, money and XP! (Axe required) DEVELOPING
+            Inv - See your items on your inventory! DEVELOPING
+            Buy - Buy some items! DEVELOPING
+            Items - See the items that you can buy! DEVELOPING
+            Stats - Show your stats! DEVELOPING
+
+
+            End - Close the game.
+            """)
+    elif option == 'HUNT':
+        m = ran(monster)
+        mlife = m[2]
+        print(f'{stats["Name"]} found {m[0]}!')
+        while True:
+            sle(1)
+            huntopt = str(input("""What will you do?
+                [ 1 ] Attack!
+                [ 2 ] Run!
+                """)[0])
+            if huntopt == '1':
+                mlife = mlife - stats['Weapon Damage']
+                life -= m[1]
+                print(f'{stats["Name"]} dealt {stats["Weapon Damage"]}')
+                print(f'{m[0]} dealt {m[1]}')
+                print(f'{stats["Name"]}\'s health is {life}')
+                print(f'{m[0]}\'s life is at {mlife}')
+            elif huntopt == '2':
+                mlife = m[3]
+                sle(1)
+                print('You ran away!')
+                break
+            if stats['Life'] <= 0:
+                print('YOU DIED!')
+                death = True
+                break
+            elif mlife <= 0:
+                print('THE MONSTER DIED!')
+                stats['xp'] += 7
+                stats['Money'] += 50
+                print(stats['Name'], f'gained 7 xp, and now is at {stats["xp"]}')
+                print(stats['Name'], f'gained 50 gold, and now is at {stats["Money"]}')
+                break
